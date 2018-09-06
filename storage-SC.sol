5e0910f121d026b1d728cd7c97284d35f757bdff
xwangsharing <41292583+xwangsharing@users.noreply.github.com>
2018-09-06 10:26:10 +0800

    update2.0
    第一次上传的主密钥部分是错的，不能将主密钥泄露的

--- storage-SC.sol
+++ storage-SC.sol
@@ -1,59 +1,59 @@
 pragma solidity ^0.4.20;
 
-contract Storage{
+contract StorageSC{
     bytes32 [] HashFileIds;
     address public owner;
-    mapping (address => User) public expireTime;
-    mapping (bytes32 =>Storaged) public sto;
+    mapping (address => User) public users;
+    mapping (bytes32 =>Storaged) public dataOwner;
     struct User{
         address userAddress;
         string interval;
+        string secretKey;
     }
     struct Storaged{
         bytes32 FileId; 
-        string MSK;
         string CT;
     }
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
-    function Storage () public {
+    function StorageSC () public {
         owner = msg.sender;
-        setMSK("","");
         setHash_FileId("");
         setCipherText("","");
     }
     function setHash_FileId(bytes32 Hash_FileId) onlyOwner public returns(bool){
          HashFileIds.push(Hash_FileId);
-         sto[Hash_FileId].FileId=Hash_FileId;
+         dataOwner[Hash_FileId].FileId=Hash_FileId;
          return true;
     }
-    function getHash_FileId(bytes32 Hash_FileId)constant public returns(bool) {
-        if(sto[Hash_FileId].FileId==Hash_FileId)
+    function checkHash_FileId(bytes32 Hash_FileId)constant public returns(bool) {
+        if(dataOwner[Hash_FileId].FileId==Hash_FileId)
         return true;
     }
-    function setMSK(bytes32 Hash_FileId,string MSK) onlyOwner public{
-        sto[Hash_FileId].MSK=MSK;
-    }
-    function getMSK(bytes32 Hash_FileId)constant public returns(string) {
-        return sto[Hash_FileId].MSK;
-    }
-    
     
     function setCipherText(bytes32 Hash_FileId,string CipherText) onlyOwner public{
-        sto[Hash_FileId].CT=CipherText;
+        dataOwner[Hash_FileId].CT=CipherText;
     }
     function getCipherText(bytes32 Hash_FileId)constant public returns(string) {
-        return  sto[Hash_FileId].CT;
+        return  dataOwner[Hash_FileId].CT;
+    }
+    
+      function setSecretKey(address useraddress,string Key) onlyOwner public{
+        users[useraddress].secretKey=Key;
+}
+    function getSecretKey() public returns(string){
+        owner = msg.sender;
+        return users[owner].secretKey;
     }
     
     function setInterval(address useraddress,string time) onlyOwner public{
-     expireTime[useraddress].interval=time;
+     users[useraddress].interval=time;
 }
     function getInterval() public returns(string){
         owner = msg.sender;
-        return expireTime[owner].interval;
+        return users[owner].interval;
     }
     
 }
